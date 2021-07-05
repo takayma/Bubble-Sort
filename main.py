1ca9fc1dfd51710126b02af6fcc3ee93d9d0c617
takayma <69343092+takayma@users.noreply.github.com>
2021-07-05 11:39:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 x = [2,3,8,1,0,-3,-12,123,22,91,111,-35,-5]
 
-def bubble_sort (arr):
+def f (arr):
 	for i in arr:
 		for j in range(len(arr) - 1):
 			if arr[j] > arr[j + 1]:
 				arr[j], arr[j + 1] = arr[j + 1], arr[j]
 	return arr
 
-print(bubble_sort(x))
+print(f(x))
